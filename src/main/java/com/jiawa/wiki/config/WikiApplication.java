e3c3ecca9cb843568be321cbf159d53bce8a5d44
piscesliu <piscesliu66@qq.com>
2022-07-27 10:20:22 +0800

    3-4 Mybatis 开发示例，测试使用 Mybatis

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,10 +1,12 @@
 package com.jiawa.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.jiawa")
+@MapperScan("com.jiawa.wiki.mapper")
 @SpringBootApplication
 public class WikiApplication {
 
