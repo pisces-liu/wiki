20e35473332c0c1393045b9d4929be005e4439bb
piscesliu <piscesliu66@qq.com>
2022-07-27 17:16:22 +0800

    4-7 封装 the-header 和 the-footer 组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,59 +1,28 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo"/>
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
-
-    <router-view />
-
-    <a-layout-footer style="text-align: center">
-      电子书
-    </a-layout-footer>
+    <the-header/>
+    <router-view/>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import TheHeader from './components/the-header.vue';
+import TheFooter from './components/the-footer.vue';
+import {defineComponent, ref} from 'vue';
+
 export default defineComponent({
   components: {
-
+    TheHeader,
+    TheFooter
   },
   setup() {
     return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      collapsed: ref<boolean>(false),
-      openKeys: ref<string[]>(['sub1']),
     };
   },
 });
 </script>
 
 <style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
 </style>
