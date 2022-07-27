cc04437ace381d0af37c1c5d99dafc26c3aeceb8
piscesliu <piscesliu66@qq.com>
2022-07-27 15:32:56 +0800

    3-7 查询 Ebook 列表

--- src/main/java/com/jiawa/wiki/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.jiawa.wiki.mapper;
-
-import com.jiawa.wiki.domain.Test;
-
-import java.util.List;
-
-public interface TestMapper {
-    public List<Test> list();
-}
---
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -0,0 +1,40 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.domain.EbookExample;
+import com.jiawa.wiki.mapper.EbookMapper;
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.resp.CommonResp;
+import com.jiawa.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Autowired
+    private EbookMapper ebookMapper;
+
+    public List<EbookResp> list(EbookReq req) {
+        // 这两行是固定写法
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        // 根据图书名称模糊查询
+        criteria.andNameLike("%" + req.getName() + "%");
+        // 返回查询的 ebook 集合
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        ArrayList<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
+    }
+}
